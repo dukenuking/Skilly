e53c2440718714be9821ab7cb0592ba9b53e8235
buzzard <6890931+buzzard@users.noreply.github.com>
2019-12-28 14:38:46 +0100

    Initial commit

---
+++ Settings.cs
@@ -0,0 +1,14 @@
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace Skilly
+{
+    public class Settings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(true);
+
+        [Menu("Enable", "enable plugin")]
+        public ToggleNode EnableCheckBox { get; set; } = new ToggleNode(true);
+    }
+}
---
+++ SkillyCore.cs
@@ -0,0 +1,44 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared;
+using ExileCore.Shared.Enums;
+using ExileCore.Shared.Helpers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Input = ExileCore.Input;
+
+namespace Skilly
+{
+    public class SkillyPluginCore : BaseSettingsPlugin<Settings>
+    {
+        public SkillyPluginCore()
+        {
+        }
+        private float currentHPAsPercent;
+        public override Job Tick()
+        {
+            GetCurrentHPAsPercent();
+           
+            if (currentHPAsPercent < 0.85f) {
+               // DebugWindow.LogMsg("Current HP is: " + currentHPAsPercent, 20f);
+                Input.KeyPressRelease(Keys.R, GameController.Window.Process.MainWindowHandle);
+            }
+            
+            return null;
+        }
+
+        private void GetCurrentHPAsPercent()
+        {
+            currentHPAsPercent = GameController.Player.GetComponent<Life>().ESPercentage;
+        }
+
+    }
+}
