fb2c344a8fa1b9dc94f175a761fe8ca79bf5be1a
buzzard <6890931+buzzard@users.noreply.github.com>
2019-12-29 13:11:47 +0100

    first version
    initial release

--- Settings.cs
+++ Settings.cs
@@ -7,8 +7,18 @@ namespace Skilly
     public class Settings : ISettings
     {
         public ToggleNode Enable { get; set; } = new ToggleNode(true);
+        [Menu("Skill hotkey")]
+        public HotkeyNode SkillHotkey { get; set; } = new HotkeyNode(System.Windows.Forms.Keys.T);
+        [Menu("Skill to use")]
+        public ListNode SkillList { get; set; } = new ListNode();
+        [Menu("Using HP")]
+        public ToggleNode EnableHp { get; set; } = new ToggleNode(false);
+        [Menu("% of HP to use skill")]
+        public RangeNode<float> PercentageToUseSkillHp { get; set; } = new RangeNode<float>(0.95f, 0.1f, 1f);
+        [Menu("Using ES")]
+        public ToggleNode EnableEs { get; set; } = new ToggleNode(false);
+        [Menu("% of HP to use skill")]
+        public RangeNode<float> PercentageToUseSkillEs { get; set; } = new RangeNode<float>(0.95f, 0.1f, 1f);
 
-        [Menu("Enable", "enable plugin")]
-        public ToggleNode EnableCheckBox { get; set; } = new ToggleNode(true);
     }
 }
---
+++ Skilly.cs
@@ -0,0 +1,72 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory.Components;
+using System.Collections.Generic;
+using System.Linq;
+using Input = ExileCore.Input;
+
+namespace Skilly
+{
+    public class Skilly : BaseSettingsPlugin<Settings>
+    {
+        public override void OnLoad()
+        {
+            PopulateSkillList();
+        }
+
+        public override Job Tick()
+        {
+            if (Settings.SkillList.Value == null) return null;
+            if (Settings.EnableHp && GetCurrentHpAsPercent() < Settings.PercentageToUseSkillHp &&
+                SkillReady(Settings.SkillList.Value))
+            {
+                CastSkill();
+            }
+
+            if (Settings.EnableEs && GetCurrentEsAsPercent() < Settings.PercentageToUseSkillEs &&
+                SkillReady(Settings.SkillList.Value))
+            {
+                CastSkill();
+            }
+
+            return null;
+        }
+
+        private bool SkillReady(string skillname)
+        {
+            //todo: add actual usage based on expired buff & cooldown or im to dumb to find it
+            // DebugWindow.LogMsg("in check");
+            return !GameController.Player.GetComponent<Actor>().ActorSkills.Find(s => s.Name.Contains(skillname))
+                .IsUsing;
+        }
+
+        private float GetCurrentEsAsPercent()
+        {
+            return GameController.Player.GetComponent<Life>().ESPercentage;
+        }
+
+        private float GetCurrentHpAsPercent()
+        {
+            return GameController.Player.GetComponent<Life>().HPPercentage;
+        }
+
+        private void PopulateSkillList()
+        {
+            var skillList = new List<string>();
+            foreach (var actorSkill in GameController.Player.GetComponent<Actor>().ActorSkills
+                .FindAll(s => s.IsOnSkillBar && s.IsUserSkill))
+            {
+                skillList.Add(actorSkill.Name);
+            }
+
+            if (skillList.Count <= 0) return;
+            Settings.SkillList.SetListValues(skillList);
+            Settings.SkillList.Value = Settings.SkillList.Values.FirstOrDefault();
+        }
+
+        private void CastSkill()
+        {
+            //default constant delay 10ms?
+            Input.KeyPressRelease(Settings.SkillHotkey, GameController.Window.Process.MainWindowHandle);
+        }
+    }
+}
